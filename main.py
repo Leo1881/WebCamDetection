bd76afe907cd312983cb78af2021c6457c6d1d36
Lee <leaveil.johnson1881@gmail.com>
2024-09-06 17:30:49 +0200

    Add email functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 import time
 import glob
+import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -9,6 +11,13 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -44,8 +53,11 @@ while True:
     status_list.append(status)
     status_list = status_list[-2:]
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-    print(status_list)
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+        email_thread.start()
 
     cv2.imshow("Video", frame)
 
@@ -55,3 +67,4 @@ while True:
         break
 
 video.release()
+clean_thread.start()
